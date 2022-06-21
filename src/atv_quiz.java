91c6e411ab68d40f9c129f50235f8bc1701e79b7
LeonardoRafaelli <leonardorafaelli@gmail.com>
2022-06-21 19:43:36 -0300

    game over

--- src/atv_quiz.java
+++ src/atv_quiz.java
@@ -8,47 +8,92 @@ public class atv_quiz {
 
 
         System.out.println("Bem vindo ao QUIZ??");
-//        System.out.println("2008, o filme aonde o tempo não existe mais:");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("fim dos tempos"));
-//
-//        System.out.println("No meu aniversário de 10 anos, preparou uma surpresa, mas fui eu.");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("eu preparei uma surpresa"));
-//
-//        System.out.println("cum morieris");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("quando voce morre"));
+        System.out.println("2008, o filme aonde o tempo não existe mais");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("fim dos tempos"));
 
-//        System.out.println("abbacsdee6f4g");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("Base64"));
+        System.out.println("No meu aniversário de 10 anos, preparou uma surpresa, mas fui eu.");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("preparei uma surpresa"));
 
-//        System.out.println("01110110 01101111 01100011 01100101 00100000 01100100 01100101 01110011 01100011 01101111 01100010 01110010 01100101 00001010");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("voce descobre"));
+        System.out.println("cum morieris");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("quando voce morre"));
 
-//        System.out.println(". -. -.-. --- -. - .-. . / .- / -.-. .... .- ...- .");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("encontre a chave"));
+        System.out.println("abbacsdee6f4g");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("Base64"));
 
-//        System.out.println("");
-//        do{
-//            System.out.print("-->: ");
-//            res = sc.nextLine();
-//        }while(!res.equalsIgnoreCase("encontre a chave"));
+        System.out.println("01110110 01101111 01100011 01100101 00100000 01100100 01100101 01110011 01100011 01101111 01100010 01110010 01100101 00001010");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("voce descobre"));
+
+        System.out.println(". -. -.-. --- -. - .-. . / .- / -.-. .... .- ...- .");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("encontre a chave"));
+
+        System.out.println("Lawrence Grey, 2015");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+            if(res.equalsIgnoreCase("hidden")){
+                System.out.println(".--. - / -... .-.");
+            }
+        }while(!res.equalsIgnoreCase("escondido"));
+
+
+        System.out.println("CIFR7A DE >CESAR<");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+            if(res.equalsIgnoreCase("cesar")){
+                System.out.println("alualuvchtlual");
+            }
+        }while(!res.equalsIgnoreCase("tente novamente"));
+
+        System.out.println("winner?");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+            if(res.equalsIgnoreCase("no")){
+                System.out.println("איזו בושה");
+            }
+        }while(!res.equalsIgnoreCase("que pena"));
+
+
+        System.out.println("Criador da Cifra de Vigenère");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+            if(res.equalsIgnoreCase("Giovan Battista Bellaso")){
+                System.out.println("Este jogo não é certo key-DESISTA");
+            } else
+                if(res.equalsIgnoreCase("Blaise de Vigenere")){
+                    System.out.println("kxlxk://b.ip.kw/2022/06/22/cgwcl6.tfo");
+                    System.out.println("Get your answers all together, answer number 4");
+                }
+        }while(!res.equalsIgnoreCase("sua desistencia"));
+
+        System.out.println("A vitória é a junção de processos bem realizados, sem que haja espaços para falha");
+        do{
+            System.out.print("-->: ");
+            res = sc.nextLine();
+        }while(!res.equalsIgnoreCase("fimdostemposprepareiumasurpresaquandovocemorrevocedescobreencontreachaveescondidotentenovamentequepenasuadesistencia"));
+
+
+        System.out.println("Estavam certos, a vitória foi feita para poucos,\npena se não conseguistes aproveitar da forma correta.");
 
     }
 }
