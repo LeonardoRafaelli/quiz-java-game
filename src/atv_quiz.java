ff720b4b82316bb9decca3c66939c5ebb91726be
LeonardoRafaelli <leonardorafaelli@gmail.com>
2022-06-21 12:18:28 -0300

    first commit

---
+++ src/atv_quiz.java
@@ -0,0 +1,54 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class atv_quiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String res;
+
+
+        System.out.println("Bem vindo ao QUIZ??");
+//        System.out.println("2008, o filme aonde o tempo não existe mais:");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("fim dos tempos"));
+//
+//        System.out.println("No meu aniversário de 10 anos, preparou uma surpresa, mas fui eu.");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("eu preparei uma surpresa"));
+//
+//        System.out.println("cum morieris");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("quando voce morre"));
+
+//        System.out.println("abbacsdee6f4g");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("Base64"));
+
+//        System.out.println("01110110 01101111 01100011 01100101 00100000 01100100 01100101 01110011 01100011 01101111 01100010 01110010 01100101 00001010");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("voce descobre"));
+
+//        System.out.println(". -. -.-. --- -. - .-. . / .- / -.-. .... .- ...- .");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("encontre a chave"));
+
+//        System.out.println("");
+//        do{
+//            System.out.print("-->: ");
+//            res = sc.nextLine();
+//        }while(!res.equalsIgnoreCase("encontre a chave"));
+
+    }
+}
